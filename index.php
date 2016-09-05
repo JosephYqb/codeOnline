eaf73c05685d65a1cb40c38e99daf815bbc58c23
JosephYqb <yqb1039961917@gmail.com>
2016-09-05 22:51:10 +0800

    ifrmae

--- index.php
+++ index.php
@@ -1,27 +1,61 @@
 <?php
 if (empty($_REQUEST)) {
-    $html = '
+ ?>
 <meta charset="UTF-8">
 <script src="http://apps.bdimg.com/libs/jquery/1.6.4/jquery.js"></script>
 <label>文件名</label><input id="filename"/>
+<label>打开方式</label>
+<input name="open_type" type='radio' value="div" id="open_type1" checked="checked"><label for="open_type1">div</label>
+<input name="open_type" type='radio' value="2"  id="open_type2" ><label for="open_type2">iframe</label>  
 <textarea  id ="content" name="content" style="width: 100%;height: 300px" onkeydown="ctlSubmit(event)">
-<?php
+&lt;?php
 </textarea>
 <button id="button"> 提交</button>
 <div id="eval"></div>
 <script>
+      function height_auto(iframe) {
+    var doc = iframe.contentWindow.document,
+      html = doc.documentElement,
+      body = doc.body;
+// 获取高度
+    var height = Math.max(body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight);
+    iframe.setAttribute('height', height);
+  }
+  </script>
+<iframe id="eval_iframe" onload=height_auto(this)></ifarme>
+<script>
     $("#button").click(
         function(){
-            $("#eval").load("",{content:$("#content").val(),filename:$("#filename").val()});
+
+        if($("[name='open_type']:checked").val() == 'div'){
+        //div 打开
+            $("#eval").load("",{content:$("#content").val(),filename:$("#filename").val(),open_type:"div"});
+        } else{
+        //iframe
+
+$.ajax({
+ type:"POST",
+ data:  {content:$("#content").val(),filename:$("#filename").val(),open_type:"iframe"} ,
+success:function(date){
+  $("#eval_iframe").attr("src",date);
+}
+})
+
+
+        }
+
         }
     );
+
     //ctrl +回车
     function ctlSubmit(event) {
         if (event.ctrlKey && event.keyCode == 13) { $("#button").click() }
     }
+
 </script>
-';
-    echo $html;
+
+<?php
 } else {
     $content  = @$_REQUEST['content'];
     if($content){
@@ -29,7 +63,13 @@ if (empty($_REQUEST)) {
         f($filename, $content);
         ini_set("display_errors", "On");
         error_reporting(E_ALL | E_STRICT);
-        include $filename;
+        if($_REQUEST['open_type']=='div'){
+         include $filename;
+        }else{
+
+        echo  $filename;
+        }
+
 }
 }
 // 文件保存函数
